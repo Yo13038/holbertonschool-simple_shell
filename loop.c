3a8104065c0fc04c0255213d4eb957f1b943557b
John-Natty <jonathanmerle8426@gmail.com>
2026-04-17 10:43:35 +0200

    Refactor shell loop by extracting command processing into a separate function

--- loop.c
+++ loop.c
@@ -1,6 +1,49 @@
 #include "main.h"
 
 /**
+ * process_line - parses and executes one command line
+ * @line: input line
+ * @env: environment variables
+ *
+ * Return: nothing
+ */
+static void process_line(char *line, char **env)
+{
+	char **argv;
+	char *path;
+	int direct_path;
+
+	argv = parse_line(line);
+	if (argv == NULL)
+		return;
+	if (argv[0] == NULL)
+	{
+		free_args(argv);
+		return;
+	}
+	if (handle_builtin(argv, env, line))
+	{
+		free_args(argv);
+		return;
+	}
+	direct_path = contains_slash(argv[0]);
+	if (direct_path)
+		path = argv[0];
+	else
+		path = find_command(argv[0]);
+	if (path == NULL)
+	{
+		write(STDERR_FILENO, "Command not found\n", 18);
+		free_args(argv);
+		return;
+	}
+	execute_command(path, argv, env);
+	if (!direct_path)
+		free(path);
+	free_args(argv);
+}
+
+/**
  * shell_loop - main loop of the shell
  * @env: environment variables
  *
@@ -9,83 +52,25 @@
 void shell_loop(char **env)
 {
 	char *line;
-	char **argv;
-	char *path;
 	size_t len;
 	ssize_t nread;
-	int direct_path;
 
 	line = NULL;
 	len = 0;
-
 	while (1)
 	{
-		/* Print the prompt only in interactive mode */
 		if (isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, "$ ", 2);
-
-		/* Read one line from standard input */
 		nread = getline(&line, &len, stdin);
 		if (nread == -1)
 		{
 			free(line);
 			break;
 		}
-
-		/* Remove the final newline character */
 		remove_newline(line);
-
-		/* Skip empty lines */
 		if (is_empty_line(line))
 			continue;
-
-		/* Split the input line into arguments */
-		argv = parse_line(line);
-		if (argv == NULL)
-			continue;
-		if (argv[0] == NULL)
-		{
-			free_args(argv);
-			continue;
-		}
-
-		/* Run builtins without creating a child process */
-		if (handle_builtin(argv, env, line))
-		{
-			free_args(argv);
-			continue;
-		}
-
-		/*
-		 * If the command already contains a slash, use it directly.
-		 * Otherwise, search for it in PATH.
-		 */
-		direct_path = contains_slash(argv[0]);
-		if (direct_path)
-			path = argv[0];
-		else
-			path = find_command(argv[0]);
-
-		/* If no valid path was found, print an error and continue */
-		if (path == NULL)
-		{
-			write(STDERR_FILENO, "Command not found\n", 18);
-			free_args(argv);
-			continue;
-		}
-
-		/* Execute the command */
-		execute_command(path, argv, env);
-
-		/*
-		 * Free path only if it was allocated by find_command.
-		 * Do not free it when it points directly to argv[0].
-		 */
-		if (!direct_path)
-			free(path);
-
-		/* Free the parsed arguments */
-		free_args(argv);
+		process_line(line, env);
 	}
 }
 
@@ -99,7 +84,6 @@ void remove_newline(char *line)
 {
 	while (*line)
 	{
-		/* Replace the newline character with the string terminator */
 		if (*line == '\n')
 		{
 			*line = '\0';
@@ -119,11 +103,9 @@ int is_empty_line(char *line)
 {
 	while (*line)
 	{
-		/* If a non-whitespace character is found, the line is not empty */
 		if (!(*line == ' ' || *line == '\t'))
 			return (0);
 		line++;
 	}
-
 	return (1);
 }
