995c0345e746711652c4f8badda200593a908a00
Yo13038 <13038@holbertonstudents.com>
2026-04-19 22:12:44 +0200

    change the basic validation of inputs

--- execute.c
+++ execute.c
@@ -3,7 +3,7 @@
 /**
  * execute_command - Executes a command in a child process.
  * @path: Full path of the executable (e.g., /bin/ls).
- * @argv: Array of arguments (e.g., {"/bin/ls", "-l", NULL}).
+ * @argv: Array of arguments (e.g., {"ls", "-l", NULL}).
  * @env: System environment variables.
  *
  * Return: 1 on success, -1 on failure.
@@ -13,32 +13,34 @@ int execute_command(char *path, char **argv, char **env)
 	pid_t child_pid;
 	int status;
 
-	/* Check if input pointers are valid */
-	if (argv == NULL || path == NULL)
+	/* Basic validation of inputs */
+	if (path == NULL || argv == NULL || argv[0] == NULL)
 		return (-1);
 
 	child_pid = fork();
 
 	if (child_pid == -1)
 	{
+		/* Fork failed */
 		perror("Fork Error");
 		return (-1);
 	}
 
 	if (child_pid == 0)
 	{
-		/* Child process: Try to execute the command */
+		/* CHILD PROCESS */
+		/* path is the full path found by find_command or provided directly */
 		if (execve(path, argv, env) == -1)
 		{
-			/* If execve fails, print error and exit child process */
 			perror("Error");
-			/*127 is commonly used to indicate "command not found" */
+			/* Exit child process with error code */
 			exit(127);
 		}
 	}
 	else
 	{
-		/* PARENT PROCESS: Wait for child to terminate */
+		/* PARENT PROCESS */
+		/* Wait for the specific child process to finish */
 		wait(&status);
 	}
 
